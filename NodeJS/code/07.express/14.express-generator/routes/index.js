c864e4fd3b76b6e70e0dce23e9f7120c31b336f7
Jixian <jixian@mackendell.com>
2024-02-24 19:22:20 +0800

    Update NodeJS Notes - 文件上传报文, express-generator

--- NodeJS/code/07.express/14.express-generator/routes/index.js
+++ NodeJS/code/07.express/14.express-generator/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+// 导入formidable
+var formidable = require('formidable');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// 显示网页的(表单)
+router.get('/portrait', (req, res) => {
+  res.render('portrait');
+});
+
+
+// 处理文件上传
+router.post('/portrait', (req, res) => {
+  // 创建表单对象
+  const form = formidable({
+    multiples: true,
+    // 设置上传文件的保存目录
+    uploadDir: __dirname + '/../public/images',
+    // 保持文件后缀
+    keepExtensions: true
+  });
+
+  // 解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // res.json({ fields, files });
+
+    // 服务器保存该图片访问URL
+    // /images/ xxx.png
+    let url  = '/images/'+ files.portrait.newFilename;  // 将来将此数据保存在数据库中
+
+    res.send(url);
+  });
+});
+
 
 module.exports = router;
